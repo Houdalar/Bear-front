8d74c8404265baf7df6e585a0ee87dfa173919b7
houda <houdadalar123@gmail.com>
2022-11-07 17:08:00 +0100

    xml

--- app/src/main/java/tn/esprit/baby_track/Login_Activity.kt
+++ app/src/main/java/tn/esprit/baby_track/Login_Activity.kt
@@ -3,9 +3,9 @@ package tn.esprit.baby_track
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Login_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
     }
 }
---
+++ app/src/main/java/tn/esprit/baby_track/models/User.kt
@@ -0,0 +1,7 @@
+package tn.esprit.baby_track.models
+
+data class User(
+    var id: Int,
+    var email: String,
+    var password: String
+)
