a8ea26e7a99c7f89769df14561251314fe52ad9e
Said-Ines <inessaid1905@gmail.com>
2022-11-07 18:19:38 +0100

    test

---
+++ app/src/main/java/tn/esprit/baby_track/SignUpActivity.java
@@ -0,0 +1,14 @@
+package tn.esprit.baby_track;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
